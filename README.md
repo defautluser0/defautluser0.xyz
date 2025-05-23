166b989876cfecd7406572c6ed8910d0520f017c
defautluser0 <urualelu0@gmail.com>
2025-05-23 18:37:01 +0200

    site source update pt1

--- README.md
+++
@@ -1,4 +0,0 @@
-# my website
-bully my terrible code
-
-todo: update the code itself to reference [the new site](https://defautluser0.xyz)
--- index.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector("button#button");
-const text = document.querySelector("#textbutton");
-
-button.addEventListener("click", () => {
-  text.toggleAttribute("hidden");
-});
